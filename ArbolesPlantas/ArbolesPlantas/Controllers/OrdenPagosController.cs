25edb738c898bbe985803c6be9c3a90e75687f06
JocelynTH <50346726+JocelynTH@users.noreply.github.com>
2020-06-21 21:53:54 -0500

    Controller y vistas de ordenes de pagos modificados

--- ArbolesPlantas/ArbolesPlantas/Controllers/OrdenPagosController.cs
+++ ArbolesPlantas/ArbolesPlantas/Controllers/OrdenPagosController.cs
@@ -21,8 +21,28 @@ namespace ArbolesPlantas.Controllers
             return View(ordenPago.ToList());
         }
 
-        // GET: OrdenPagos/Details/5
-        public ActionResult Details(int? id)
+		// GET: OrdenPagos
+		public ActionResult historialPagos()
+		{
+			var ordenPago = db.ordenPago.Include(o => o.Factura);
+			var orden = (from o in db.ordenPago
+						 where o.status == 1
+						 select o).ToList();
+			return View(orden);
+		}
+
+		// GET: OrdenPagos
+		public ActionResult pagosPendientes()
+		{
+			var ordenPago = db.ordenPago.Include(o => o.Factura);
+			var orden = (from o in db.ordenPago
+						 where o.status == 0
+						 select o).ToList();
+			return View(orden);
+		}
+
+		// GET: OrdenPagos/Details/5
+		public ActionResult Details(int? id)
         {
             if (id == null)
             {
@@ -37,11 +57,38 @@ namespace ArbolesPlantas.Controllers
         }
 
         // GET: OrdenPagos/Create
-        public ActionResult Create()
+        public ActionResult Create(int? id)
         {
-            ViewBag.id_factura = new SelectList(db.Factura, "Id", "Id");
-            return View();
-        }
+			//ViewBag.id_factura = new SelectList(db.Factura, "Id", "Id");
+			//return View();
+
+
+			//if (User.Identity.IsAuthenticated)
+			//{
+			//	if (User.IsInRole("PagoProveedores"))
+			//	{
+					
+					Factura factura = db.Factura.Find(id);
+					ordenPago o = new ordenPago();
+					o.id_factura = factura.Id;
+					o.cantidad_pago = factura.total;
+					o.cantidad_restante = 0;
+
+					return View(o);
+					
+			//	}
+			//	else
+			//	{
+			//		System.Diagnostics.Debug.WriteLine("No Autentificado otro rol");
+			//		return RedirectToAction("sinPrivilegios", "Home");
+			//	}
+			//}
+			//else
+			//{
+			//	System.Diagnostics.Debug.WriteLine("No Autentificado");
+			//	return RedirectToAction("Login", "Account");
+			//}
+		}
 
         // POST: OrdenPagos/Create
         // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
@@ -50,16 +97,29 @@ namespace ArbolesPlantas.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,fecha_pago,id_factura,cantidad_pago,cantidad_restante,status")] ordenPago ordenPago)
         {
-            if (ModelState.IsValid)
-            {
-                db.ordenPago.Add(ordenPago);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+			DateTime dateTime = DateTime.Today;
 
-            ViewBag.id_factura = new SelectList(db.Factura, "Id", "Id", ordenPago.id_factura);
-            return View(ordenPago);
-        }
+			System.Diagnostics.Debug.WriteLine(dateTime.ToString("dd/MM/yyyy"));
+			System.Diagnostics.Debug.WriteLine("Fecha pago" + ordenPago.fecha_pago);
+			System.Diagnostics.Debug.WriteLine("ID fatura" + ordenPago.id_factura);
+			System.Diagnostics.Debug.WriteLine("Cantidad pago" + ordenPago.cantidad_pago);
+			System.Diagnostics.Debug.WriteLine("Cantidad restante" + ordenPago.cantidad_restante);
+			System.Diagnostics.Debug.WriteLine("Status " + ordenPago.status);
+			ordenPago.fecha_pago = dateTime;
+			ordenPago.status = 0;
+			ordenPago.cantidad_restante = 0;
+
+			db.ordenPago.Add(ordenPago);
+			db.SaveChanges();
+
+			//status de factura 0: no pagada, 1: esperando aprobaciòn, 2: aprobada, 3: rechazada
+			Factura facturaP = db.Factura.Find(ordenPago.id_factura);
+			facturaP.status = 1;
+			db.Entry(facturaP).State = EntityState.Modified;
+			db.SaveChanges();
+
+			return RedirectToAction("facturasPendientes", "Facturas");
+		}
 
         // GET: OrdenPagos/Edit/5
         public ActionResult Edit(int? id)
