9a52ea6865907a5156fff0c9c57a97ae2ddae09e
JocelynTH <50346726+JocelynTH@users.noreply.github.com>
2020-06-21 19:29:53 -0500

    proyecto sin base de datos

---
+++ ArbolesPlantas/ArbolesPlantas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ArbolesPlantas.MvcApplication" Language="C#" %>
---
+++ ArbolesPlantas/ArbolesPlantas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArbolesPlantas.Startup))]
+namespace ArbolesPlantas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
