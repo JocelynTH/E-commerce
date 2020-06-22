1c296722881f6498e6a4b4f6ab27fd7a55d1bb55
JocelynTH <50346726+JocelynTH@users.noreply.github.com>
2020-06-21 22:01:42 -0500

    Creación de roles en startup

--- ArbolesPlantas/ArbolesPlantas/Startup.cs
+++ ArbolesPlantas/ArbolesPlantas/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using ArbolesPlantas.Models;
 
 [assembly: OwinStartupAttribute(typeof(ArbolesPlantas.Startup))]
 namespace ArbolesPlantas
@@ -9,6 +12,59 @@ namespace ArbolesPlantas
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+			CreaRoles();
         }
-    }
+
+		private void CreaRoles()
+		{
+			ApplicationDbContext applicationDbContext = new ApplicationDbContext();
+			var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(applicationDbContext));
+
+			var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(applicationDbContext));
+
+			if (!roleManager.RoleExists("Admin"))
+			{
+				//Rol para el superadmin
+				var role = new IdentityRole();
+				role.Name = "Admin";
+				roleManager.Create(role);
+
+			}
+			if (!roleManager.RoleExists("Compras"))
+			{
+				var role = new IdentityRole();
+				role.Name = "Compras";
+				roleManager.Create(role);
+
+			}
+			if (!roleManager.RoleExists("Ventas"))
+			{
+				var role = new IdentityRole();
+				role.Name = "Ventas";
+				roleManager.Create(role);
+
+			}
+			if (!roleManager.RoleExists("PagoProveedores"))
+			{
+				var role = new IdentityRole();
+				role.Name = "PagoProveedores";
+				roleManager.Create(role);
+
+			}
+			if (!roleManager.RoleExists("Finanzas"))
+			{
+				var role = new IdentityRole();
+				role.Name = "Finanzas";
+				roleManager.Create(role);
+			}
+			if (!roleManager.RoleExists("AlmacenE"))
+			{
+				var role = new IdentityRole();
+				role.Name = "AlmacenE";
+				roleManager.Create(role);
+			}
+		}
+	}
+
+
 }
